806053e40bc6bff3d79413191a55984894410800
Ahmad Fauzan <72255323+absort-io@users.noreply.github.com>
2020-10-14 21:52:23 +0800

    Update praktikum1nomor3.java
    Perbaikan, karena lupa commit pada perbaikan sebelumnya

--- H071201001/Praktikum 1/praktikum1nomor3.java
+++ H071201001/Praktikum 1/praktikum1nomor3.java
@@ -1,31 +1,15 @@
 import java.util.Scanner;
 public class praktikum1nomor3 {
     public static void main(String[] args) {
-        int H,A,B;
-        do
-        {
-            Scanner data = new Scanner(System.in);
-            System.out.print("Tinggi Menara (h) = ");
-            H = data.nextInt();
-            System.out.print("Sudut ke bagian belakang kapal (a) = ");
-            A = data.nextInt();
-            System.out.print("Sudut ke bagian depan kapal (b) = ");
-            B = data.nextInt();
-                if (B>90 || B>A)
-                {
-                    System.out.println("\nInput TIDAK valid! Mohon masukkan ulang nilai sesuai interval: (90>a>b)\n");
-                }
-        } 
-        while(B>90 || B>A);
+        Scanner data = new Scanner(System.in);
+        int H = data.nextInt();
+        int A = data.nextInt();
+        int B = data.nextInt();
 
-        double sina = Math.sin(Math.toRadians(A));
-        double sinadiv = Math.sin(Math.toRadians(180-90+A));
-        double sinb = Math.sin(Math.toRadians(B));
-        double sinbdiv = Math.sin(Math.toRadians(180-90+B));
-
-        double front = (sinb*H)/sinbdiv;
-        double back = (sina*H)/sinadiv;
-        double panjang = back-front;
-        System.out.printf("Panjang Kapal = %.1f m\n", panjang);
+        double tanA = Math.Tan(Math.toRadians(A))*H;
+        double tanB = math.Tan(math.toRadians(B))*H;
+        
+        double panjangKapal = tanA-tanB;
+        System.out.printf("Panjang Kapal = %.1f m\n", panjangKapal);
     }
 } 
