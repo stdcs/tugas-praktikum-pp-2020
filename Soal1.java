91ea60462560e222bd4b8e2f0c0263644d0b4c6e
ezayahya511 <72252233+ezayahya511@users.noreply.github.com>
2020-10-15 17:27:59 +0800

    Praktikum 1

---
+++ Soal1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner; 
+public class Soal1 {
+    static Scanner answer = new Scanner(System.in);
+public static void main(String[] args) {
+    System.out.println("jam");
+    int a = answer.nextInt();
+    System.out.println("km/jam");
+    int b = answer.nextInt();
+    float bensin = (float) b / 14 * a;
+    System.out.printf("%.3f", bensin);
+
+
+
+}        
+      }
---
+++ Soal2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Soal2 {
+static Scanner answer = new Scanner(System.in);
+public static void main(String[] args) {
+    System.out.println("jumlah detik");
+    int a = answer.nextInt();
+    int jam = a / 3600;
+    int menit = a / 60 % 60;
+    int detik = a % 60;
+    
+    System.out.printf("%02d:%02d:%02d", jam, menit, detik);
+
+    }
+}
