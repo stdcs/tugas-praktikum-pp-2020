f9be9f72c8ed3651cf2ecb55bf2a8cca80fcc3bf
khalsum <khalsum.uk@gmail.com>
2020-11-06 00:07:48 +0800

    Praktikum 1, 2, dan 3

--- H071201072/PRAKTIKUM-3/Nomor4.java
+++ H071201072/PRAKTIKUM-3/Nomor4.java
@@ -15,15 +15,37 @@ public class Nomor4 {
             int rp2k = 0;
             int rp1k = 0; 
 
-            while (kembalian>0){
-                kembalian >=
+            while (kembalian > 0) {
+                if (kembalian >= 100000) {
+                    kembalian -= 100000;
+                    rp100k++;
+                } else if (kembalian >= 50000) {
+                    kembalian -= 50000;
+                    rp50k++;
+                } else if (kembalian >= 20000) {
+                    kembalian -= 20000;
+                    rp20k++;
+                } else if (kembalian >= 10000) {
+                    kembalian -= 10000;
+                    rp10k++;
+                } else if (kembalian >= 5000) {
+                    kembalian -= 5000;
+                    rp5k++;
+                } else if (kembalian >= 2000) {
+                    kembalian -= 2000;
+                    rp2k++;
+                } else {
+                    kembalian -= 1000;
+                    rp1k++;
+                }
             }
-                
-
-                
-
-
-                
-
-} 
-}
+        System.out.println(rp100k+ " uang Rp. 100.000");
+        System.out.println(rp50k+ " uang Rp. 50.000");
+        System.out.println(rp20k+ " uang Rp. 20.000");
+        System.out.println(rp10k+ " uang Rp. 10.000");
+        System.out.println(rp5k+ " uang Rp. 5.000");
+        System.out.println(rp2k+ " uang Rp. 2.000");
+        System.out.println(rp1k+ " uang Rp. 1.000");
+        }
+    }
+    
