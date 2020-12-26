6fb9d3aaaef663cbb28859c534a75dd494bf7414
averinagracelia <averinagrc@gmail.com>
2020-12-26 23:32:00 +0800

    Praktikum5

---
+++ H071201034/Praktikum5/Nomor3.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Nomor3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int totalDay = input.nextInt();
+
+        System.out.println(myDay(totalDay));
+        input.close();
+    }
+
+    public static String myDay(int d) {
+        int year, month, day;
+
+        year = d / 365;
+        d = d % 365;
+
+        month = d / 30;
+        d = d % 30;
+
+        day = d;
+
+        return (year + " Tahun \n" + month + " Bulan \n" + day + " Hari \n");
+    }
+}
